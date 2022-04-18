df6566d3f4fef1f5a6af99ed29bd48f494318f2b
SandBot <super.khang2@gmail.com>
2022-04-18 21:38:28 +0700

    yf
    fh

--- Game/GameMain.java
+++ Game/GameMain.java
@@ -2,6 +2,8 @@ package Game;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
 public class GameMain extends Application {
@@ -18,6 +20,11 @@ public class GameMain extends Application {
 
         gameHandler = new GameHandler();
 
+        String musicFile = "Tranceition.mp4";     // For example
+
+        Media sound = new Media(getClass().getResource(musicFile).toExternalForm());
+        MediaPlayer mediaPlayer = new MediaPlayer(sound);
+        mediaPlayer.play();
 
         primaryStage.setTitle("BoomBoom");
         primaryStage.setWidth(screenWidth);
